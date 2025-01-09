447691f22aae2002be153f573e15e8480366f690
Mark Bennett N. Pineda <markbennettpineda@gmail.com>
2025-01-10 04:38:19 +0800

    Windows 95 Design Applying

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,38 @@ module.exports = {
     // Add more paths where HTML or EJS files are located
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "title-font-family": "W95Fa-Regular, sans-serif",
+        "font-title": "W95Fa-Regular, sans-serif",
+      },
+      fontSize: {
+        "title-font-size": "12px",
+        "size-title": "12px",
+      },
+      fontWeight: {
+        "title-font-weight": "400",
+      },
+      lineHeight: {
+        "title-line-height": "13px",
+        "line-height-title": "13px",
+      },
+      letterSpacing: {
+        "letter-spacing-title": "0.22px",
+      },
+      borderRadius: {},
+      colors: {
+        "semantic-backgroud-navy": "#000080",
+        "semantic-text-inverse": "#ffffff",
+        "basic-neutral-silver": "#c0c0c0",
+      },
+      spacing: {},
+      width: {},
+      minWidth: {},
+      maxWidth: {},
+      height: {},
+      minHeight: {},
+      maxHeight: {},
+    },
   },
-  plugins: [],
 };
