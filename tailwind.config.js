2e9284c26397ad8aa168269a4c0db2cbb5a710d7
Mark Bennett N. Pineda <markbennettpineda@gmail.com>
2025-01-12 23:36:35 +0800

    Cleaning code

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,19 @@ module.exports = {
   ],
   theme: {
     extend: {
+      boxShadow: {
+        'outside-3': `
+          inset -2px -2px 0px 0px rgba(129, 129, 129, 1),
+          inset 1px 1px 0px 0px rgba(255, 255, 255, 1),
+          inset -1px -1px 0px 0px rgba(0, 0, 0, 1)
+        `,
+       'outside-full': `
+          inset -1px -1px 0px #000000,
+          inset 1px 1px 0px #C1C1C1,
+          inset -2px -2px 0px #818181,
+          inset 2px 2px 0px #FFFFFF
+        `,
+      },
       fontFamily: {
         "title-font-family": "W95Fa-Regular, sans-serif",
         "font-title": "W95Fa-Regular, sans-serif",
